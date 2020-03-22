8b50f7a51b604ce123c24274f7359bb7aa1928cf
hseka <sekaherve@gmail.com>
2020-03-22 17:16:48 +0100

    Ajout d'une Route pour la vue "customers"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,10 +12,12 @@ class HomeController extends Controller
      *
      * @return void
      */
+    /*
     public function __construct()
     {
         $this->middleware('auth');
     }
+    */
 
     /**
      * Show the application dashboard.
@@ -23,6 +26,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $users = User::all();
+        return view('excel.customers', compact('users'));
     }
 }
