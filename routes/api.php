5d3279daf74e2d8d75a5d2f16eb4d24d87e25812
hseka <sekaherve@gmail.com>
2020-10-01 15:47:59 +0200

    Modification de la Route de l'API avec l'option d'authentification

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('topicality', 'TopicalityController');
+// Route::apiResource('topicality', 'TopicalityController');
+
+Route::middleware('auth:api')->group(function() {
+    Route::apiResource('topicality', 'TopicalityController');
+});
+
+
